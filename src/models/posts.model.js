7db872afc9f69417dae66269c2e0b3c0fbc6f0d3
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2018-02-17 10:30:50 +0700

    61.posts-services

---
+++ src/models/posts.model.js
@@ -0,0 +1,49 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+module.exports = function (app) {
+  const sequelizeClient = app.get('sequelizeClient');
+  const posts = sequelizeClient.define('posts', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    location: {
+      type: Sequelize.STRING
+    },
+    image: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    likes: {
+      type: Sequelize.INTEGER,
+      defaultValue: 0
+    },
+
+    //relation
+    createdBy: {
+      type: Sequelize.INTEGER
+    },
+
+    // Timestamps
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE,
+  }, {
+    hooks: {
+      beforeCount(options) {
+        options.raw = true;
+      }
+    }
+  });
+
+  posts.associate = function (models) { // eslint-disable-line no-unused-vars
+    // Define associations here
+    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+  };
+
+  return posts;
+};
--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,8 @@
 const todos = require('./todos/todos.service.js');
 const users = require('./users/users.service.js');
+const posts = require('./posts/posts.service.js');
 module.exports = function (app) {
   app.configure(todos);
   app.configure(users);
+  app.configure(posts);
 };
