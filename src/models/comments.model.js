17f857f9c3e519f29a61c5cbe33da66ad8ebd43f
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2018-02-17 10:35:43 +0700

    62. comments service

---
+++ src/models/comments.model.js
@@ -0,0 +1,44 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+module.exports = function (app) {
+  const sequelizeClient = app.get('sequelizeClient');
+  const comments = sequelizeClient.define('comments', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    comment: {
+      type: Sequelize.TEXT
+    },
+
+    //relation
+    postId: {
+      type: Sequelize.INTEGER
+    },
+    createdBy: {
+      type: Sequelize.INTEGER
+    },
+
+    // Timestamps
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE,
+  }, {
+    hooks: {
+      beforeCount(options) {
+        options.raw = true;
+      }
+    }
+  });
+
+  comments.associate = function (models) { // eslint-disable-line no-unused-vars
+    // Define associations here
+    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+  };
+
+  return comments;
+};
--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,10 @@
 const todos = require('./todos/todos.service.js');
 const users = require('./users/users.service.js');
 const posts = require('./posts/posts.service.js');
+const comments = require('./comments/comments.service.js');
 module.exports = function (app) {
   app.configure(todos);
   app.configure(users);
   app.configure(posts);
+  app.configure(comments);
 };
